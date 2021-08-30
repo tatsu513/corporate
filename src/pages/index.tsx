cf62a486ab53c236a1b87eceed3af0695dde35e4
tatsu513 <0285mashiko@gmail.com>
2021-08-30 22:42:41 +0900

    トップにリンクを追加、sanouにadoutを追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,15 @@
 import { GetStaticProps } from 'next';
+import { useRouter } from 'next/dist/client/router';
 import styles from 'styles/modules/Home.module.scss';
 
 export const Home: React.VFC = () => {
-  return <div className={styles.topWrap}>トップ</div>;
+  const router = useRouter();
+  return (
+    <div className={styles.topWrap}>
+      <div onClick={() => router.push('/unou')}>右脳</div>
+      <div onClick={() => router.push('/sanou')}>左脳</div>
+    </div>
+  );
 };
 
 export const getStaticProps: GetStaticProps = async () => {
