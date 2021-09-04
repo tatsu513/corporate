c62a106a96b14b624fd27d414d861ab6617c320c
tatsu513 <0285mashiko@gmail.com>
2021-09-04 13:05:14 +0900

    全体にフォントを設定

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,13 @@
+import Image from 'next/image';
+import logo from 'images/kikutokaku_logo.svg';
 import styles from 'styles/modules/Header.module.scss';
 
 const Header: React.VFC = () => {
   return (
     <header>
       <div className={styles.headerWrap}>
-        <div className={styles.logo}>聴くと描く</div>
-        <div className={styles.menu}>
-          <span
-            className={`${styles.menuBar} ${styles.menubarTop}`}
-          />
-          <span
-            className={`${styles.menuBar} ${styles.menubarMiddle}`}
-          />
-          <span
-            className={`${styles.menuBar} ${styles.menubarBottom}`}
-          />
+        <div className={styles.logo}>
+          <Image src={logo} alt='聴くと描く' />
         </div>
       </div>
     </header>
