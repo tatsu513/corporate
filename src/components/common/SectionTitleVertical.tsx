c86acd14558f3943bf6a8b2fbe9357b55639b704
tatsu513 <0285mashiko@gmail.com>
2021-09-14 23:15:05 +0900

    about/newsアニメーション追加

--- src/components/common/SectionTitleVertical.tsx
+++ src/components/common/SectionTitleVertical.tsx
@@ -1,3 +1,4 @@
+import { useInView } from 'react-intersection-observer';
 import styles from 'styles/modules/SectionTitleVertical.module.scss';
 
 interface Props {
@@ -8,13 +9,22 @@ interface Props {
 }
 
 const SectionTitleVertical: React.VFC<Props> = (props) => {
+  const [titleRef, inView] = useInView({
+    rootMargin: '-50px 0px',
+    triggerOnce: true,
+  });
   return (
     <div
       className={`${styles.titleWrap} ${
         props.isWhite && styles.white
-      }`}
+      } ${inView && styles.inView}`}
     >
-      <h2 className={styles.title}>{props.title}</h2>
+      <h2
+        ref={titleRef}
+        className={`${styles.title} ${inView && styles.inView}`}
+      >
+        {props.title}
+      </h2>
       {props.subTitle && (
         <div className={styles.subTitleWrap}>
           <span className={styles.subTitle}>{props.subTitle}</span>
