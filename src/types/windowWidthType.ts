9961f30d2c8c6bd9e59bcef6bff7ab69602ebaee
大川達也 <tatsuya.okawa@1-2-283.local>
2023-11-02 23:22:01 +0900

    update

---
+++ src/types/windowWidthType.ts
@@ -0,0 +1,3 @@
+import { WINDOW_WIDTH_TIPE } from "constants/windowWidthType";
+
+export type WindowWidthType =  typeof WINDOW_WIDTH_TIPE.sm | typeof WINDOW_WIDTH_TIPE.md | typeof WINDOW_WIDTH_TIPE.lg
